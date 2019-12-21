e6d648583d336ce01137bb64758dc3cd423e800a
ritik singh <mr.ritik99@gmail.com>
2019-12-21 15:12:41 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and build a ecommerce.
         </a>
       </header>
     </div>
